9a882817d5ff2043a6afc65b61963f54860e1445
opusmagnum77 <daniel.adeyanju@gmail.com>
2016-03-26 07:26:45 -0400

    create evnet

--- create_event.js
+++ create_event.js
@@ -1,6 +1,3 @@
-/**
- * Created by dan on 3/26/16.
- */
 Parse.initialize("1Nme1kGOwimbGs2cVfNS8wj2jpYnMUblNgdTQsl2", 'cvxX7RpSbkm2fPs19auYbgPPE0Y4bbVphqbohnHg');
 //Parse.serverURL = 'http://localhost:1337/parse';
 
@@ -9,25 +6,31 @@ window.onload = function(){
 
 };
 
-$( "#signup-btn" ).click(function() {
-    createUser();
+$( "#submit" ).click(function() {
+    createEvent();
 });
 
-function createUser(){
-    var username = $('#username').val();
-    var password = $('#password').val();
-    var password = $('#ver-password').val();
+function createEvent() {
+    //eventName location birthDate description eventDate goal charity
+    var location = $('#location').val();
+    var eventName = $('#eventName').val();
+    var birthDate = $('#birthDate').val();
+    var birthDate = $('#description').val();
+    var birthDate = $('#eventDate').val();
+    var birthDate = $('#goal').val();
+    var birthDate = $('#charity').val();
+
 
     var user = new Parse.User();
     user.set("username", username);
     user.set("password", password);
 
     user.signUp(null, {
-        success: function(user) {
+        success: function (user) {
             // Hooray! Let them use the app now.
             alert('yay');
         },
-        error: function(user, error) {
+        error: function (user, error) {
             // Show the error message somewhere and let the user try again.
             alert("Error: " + error.code + " " + error.message);
         }
